cfc534f27ddbd349b00729cceb4304c294bd2cb5
aankor <lambdasound@yandex.ru>
2021-07-29 20:57:04 +0300

    Config cluster in create_self_upgrade_multisig.sh

--- scripts/create_self_upgrade_multisig.sh
+++ scripts/create_self_upgrade_multisig.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-multisig --cluster d create-multisig --multisig-account ../keys/self_upgrade_multisig.json --output-multisig-pda ../target/self_upgrade_multisig_pda.pubkey  --threshold $1 --owner ${@:2}
+multisig --cluster $1 create-multisig --multisig-account ../keys/self_upgrade_multisig.json --output-multisig-pda ../target/self_upgrade_multisig_pda.pubkey  --threshold $2 --owner ${@:3}
